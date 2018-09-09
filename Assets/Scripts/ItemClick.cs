5185ef5c6ec7ce5954b7ca124d9ec005f5971b17
kimyoungsun <kyssmart@naver.com>
2018-09-09 13:54:11 +0900

    222
    222

---
+++ Assets/Scripts/ItemClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemClick : MonoBehaviour {
+	
+	void OnPress (bool _bDown){
+		Debug.Log (_bDown);
+		OnDestroy ();
+	}
+
+	void OnDestroy(){
+		gameObject.SetActive (false);
+	}
+}
