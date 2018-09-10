5eecfc1d8a4238c5f1ca21f2d3229e9491ca583b
kimyoungsun <kyssmart@naver.com>
2018-09-10 10:21:12 +0900

    22
    22

--- Assets/Scripts/ItemClick.cs
+++ Assets/Scripts/ItemClick.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemClick : MonoBehaviour {
-	
+
+	//void OnMouseDown(){
+	//
+	//}
+
+	//void OnPress
+
 	void OnPress (bool _bDown){
 		Debug.Log (_bDown);
 		OnDestroy ();
+	}
+
+	void OnClick(){
+		//On
 	}
 
 	void OnDestroy(){
